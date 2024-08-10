36bd803db0a44f4e51a9a374e3222b01bb43a4eb
Vedant Tiwari <vedanttiwari9685@gmail.com>
2024-08-10 17:36:33 +0530

    file position changed

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreen();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dymanic"),
+      ),
+    );
+  }
+}
