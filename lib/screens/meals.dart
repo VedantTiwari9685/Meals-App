48f875faa51cd430625c4b152779f951cbb16a54
Vedant Tiwari <vedanttiwari9685@gmail.com>
2024-08-10 19:16:30 +0530

    Bottom Nevigation Bar

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
-import 'package:meals/widgets/meal_details.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -56,9 +56,11 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+
+    if (title == null) return content;
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
   State<TabsScreen> createState() {
-    return _TabsScreen();
+    return _TabsScreenState();
   }
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var activePageTitle = "Category";
+    Widget activeScreen = const CategoriesScreen();
+
+    if (_selectedPageIndex == 1) {
+      activePageTitle = "Your Favorites";
+      activeScreen = const MealsScreen(meals: []);
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dymanic"),
+        title: Text(activePageTitle),
+      ),
+      body: activeScreen,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.food_bank), label: "Categories"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites"),
+        ],
       ),
     );
   }
