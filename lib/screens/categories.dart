dfe6543150b39e73fa85b6f143d4a71c876e8748
Vedant Tiwari <vedanttiwari9685@gmail.com>
2024-08-10 11:37:24 +0530

    Meal pics with names using Card widget

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    ); // Navigator.push(context, route) both same
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +41,9 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
             )
         ],
       ),
